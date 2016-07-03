bfc5c5a4acb3a80a1719f865e9fc11bb08501d79
soniloi <niloinso@tcd.ie>
2016-07-03 19:09:52 +0100

    Added more information and output

--- hmmm/hmmm.py
+++ hmmm/hmmm.py
@@ -3,7 +3,14 @@
 import time
 import datetime
 
-RETIREMENT_AGE = 65 # TODO: per-country
+SECONDS_PER_DAY = 86400
+DAYS_PER_WEEK = 7
+WORKING_DAYS_PER_WEEK = 5
+
+RETIREMENT_AGE = 68 # TODO: per-country
+PUBLIC_HOLIDAYS_MONDAY = 5 # Number of public holidays each year that always fall on a Mondays
+PUBLIC_HOLIDAYS_OTHER = 4 # Number of public holidays each year that may fall on any day
+ANNUAL_LEAVE_DAYS = 20
 
 dob_cal = raw_input("What is your date of birth (dd/mm/yyyy)? ").split("/")
 dob_year = int(dob_cal[2])
@@ -14,7 +21,15 @@ dob_unix = time.mktime(datetime.date(dob_year, dob_month, dob_day).timetuple())
 dor_unix = time.mktime(datetime.date(dob_year + RETIREMENT_AGE, dob_month, dob_day).timetuple())
 today_unix = time.time()
 
-print "dob: " + str(dob_unix)
-print "dor: " + str(dor_unix)
-print "today: " + str(today_unix)
-print "seconds until retirement: " + str(dor_unix - today_unix)
+seconds_until = dor_unix - today_unix
+days_until = seconds_until / SECONDS_PER_DAY
+working_days_until = days_until * WORKING_DAYS_PER_WEEK / DAYS_PER_WEEK
+
+print "Key dates:"
+print "\tdob: " + str(dob_unix)
+print "\tdor: " + str(dor_unix)
+print "\ttoday: " + str(today_unix)
+print "Until retirement:"
+print "\tseconds: " + str(seconds_until)
+print "\tdays: " + str(days_until)
+print "\tworking days: " + str(working_days_until)
